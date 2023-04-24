99201afc9ae4949655bf2e25450e5a12c7e0bbb2
dam1t <po2032@gmail.com>
2023-04-24 19:03:07 +0200

    2do Commit Creado TEST (canal origen: 1, canal destino 2, Salida: 1)

--- src/ZappingTest.java
+++ src/ZappingTest.java
@@ -13,4 +13,13 @@ class ZappingTest {
         //ASSERT
         assertEquals(0,resultado);
     }
+    @Test
+    void OrigenUnoDestinoDosSalidaUno(){
+        //ARRANGE
+        Zapping salida = new Zapping();
+        //ACT
+        int resultado = salida.canales(1,2);
+        //ASSERT
+        assertEquals(1,resultado);
+    }
 }
